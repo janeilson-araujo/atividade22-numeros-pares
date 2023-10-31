3d76611e2779058541903aac470466a4b0c1f1bc
janeilson-araujo <janeilsonpaulino013@gmail.com>
2023-10-31 16:24:58 -0300

    atividade22 numeros pares python

---
+++ numeros_pares.py
@@ -0,0 +1,5 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+
+print("numeros pares de 1 a 50")
+for numeros in range(0, 51, 2):
+    print(numeros)
